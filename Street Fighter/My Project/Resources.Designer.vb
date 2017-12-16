114622b8a74169d86b30a618c550a68867649a9f
Predrika Br Ginting <ichapredrikagm@gmail.com>
2017-12-17 02:02:30 +0700

    -belom selesai

--- Street Fighter/My Project/Resources.Designer.vb
+++ Street Fighter/My Project/Resources.Designer.vb
@@ -162,6 +162,76 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property beed0() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("beed0", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property beed1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("beed1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property beed2() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("beed2", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property beed3() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("beed3", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property beed4() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("beed4", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property beed5() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("beed5", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property beed6() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("beed6", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property beeR0() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("beeR0", resourceCulture)
